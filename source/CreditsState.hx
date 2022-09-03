27abc1220ed7454b9ddd2307a5824636d73a31a0
Randomlol1 <112783310+Randomlol1@users.noreply.github.com>
2022-09-03 18:13:30 -0500

    Add files via upload

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -81,6 +81,8 @@ class CreditsState extends MusicBeatState
 		#end
 
 		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
+		    ['B-sida Rewritten Android Port'],
+		    ['Random',	'random',				'EEEEEE',					'https://youtube.com/channel/UCNnNGZIB6TvB9E2TstMqCyQ', 	'F73838'],
 			['Psych Engine Android'],
 			['Saw (M.A. Jigsaw)',	'saw',				'Main Programmer of Psych Engine Android',					'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ', 	'F73838'],
 			[''],
